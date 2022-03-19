33c7f17dad3ab7cb23b5272daf3117178942963f
Ismail Alfiyasin <ismailalfiyasin6@gmail.com>
2022-03-19 22:35:04 +0700

    67 Major Upgrade

--- go.mod
+++ go.mod
@@ -1,3 +1,7 @@
 module github.com/ismail118/app-say-hello
 
 go 1.17
+
+require github.com/ismail118/go-say-hello v1.0.1
+
+require github.com/ismail118/go-say-hello/v2 v2.0.0 // indirect
--- main.go
+++ main.go
@@ -1,5 +1,18 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	go_say_hello "github.com/ismail118/go-say-hello"
 
+	go_say_hellov2 "github.com/ismail118/go-say-hello/v2"
+)
+
+/*
+	for more
+	see documentation https://docs.gofiber.io/
+*/
+
+func main() {
+	fmt.Println(go_say_hello.SayHello())
+	fmt.Println(go_say_hellov2.SayHello("Ismail"))
 }
